29f37ab33e4a98d3f0032decc3ccf59556d49982
Stikerz <abiola_saraki@live.co.uk>
2020-09-12 04:00:51 +0100

    add views to url

---
+++ weddingshop/wedding_list/urls.py
@@ -0,0 +1,19 @@
+from django.urls import path
+
+from wedding_list.views import (
+    WeddingListCAPIView,
+    WeddingListRUDAPIView,
+    ProductAPIView,
+    UserAPIView,
+    UserDetailView,
+    ProductRUDAPIView
+)
+
+urlpatterns = [
+    path("users/", UserAPIView.as_view(), name="user_list"),
+    path("users/<int:pk>/", UserDetailView.as_view(), name="user_detail"),
+    path("products/", ProductAPIView.as_view(), name="product_list"),
+    path("product/<int:pk>/", ProductRUDAPIView.as_view(), name="product_detail"),
+    path("list/", WeddingListCAPIView.as_view(), name="wedding_list"),
+    path("listitem/<int:pk>/", WeddingListRUDAPIView.as_view(), name="wedding_item"),
+]
