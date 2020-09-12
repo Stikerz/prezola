b6e0cb3437e5763d0ff5334b734b1fcb57271704
Stikerz <abiola_saraki@live.co.uk>
2020-09-12 04:19:32 +0100

    add profile component

---
+++ src/src/app/components/profile/profile.component.ts
@@ -0,0 +1,55 @@
+import { Component, OnInit } from '@angular/core';
+import {UserService} from '../../services/user.service';
+import {ToastrService} from 'ngx-toastr';
+import {Router} from '@angular/router';
+
+@Component({
+  selector: 'app-profile',
+  templateUrl: './profile.component.html',
+  styleUrls: ['./profile.component.css']
+})
+export class ProfileComponent implements OnInit {
+
+  password = '';
+  token: string;
+  firstname: string;
+  lastname: string;
+
+  constructor(private userService: UserService, private toastr: ToastrService,  private router: Router) {}
+
+  ngOnInit(): void {
+        this.userService.sharedToken.subscribe(data => this.token = data);
+        if (this.token) {
+      this.userService.getUser();
+      this.userService.sharedUser.subscribe(data => {
+        this.firstname = data.first_name;
+        this.lastname = data.last_name;
+      });
+    } else {
+      this.router.navigate(['login']);
+    }
+  }
+
+  update() {
+    const data: object = {};
+    if (this.firstname) {
+      // @ts-ignore
+      data.first_name = this.firstname;
+
+    }
+    if (this.lastname) {
+      // @ts-ignore
+      data.last_name = this.lastname;
+
+    }
+    if (this.password) {
+      // @ts-ignore
+      data.password = this.password;
+    }
+    this.userService.updateUser(data);
+    }
+
+    delete() {
+    this.userService.deleteUser();
+    }
+}
