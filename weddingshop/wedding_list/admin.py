0572e04a1e0de6159a62a16f8979f5f317d885f6
Stikerz <abiola_saraki@live.co.uk>
2020-09-12 04:01:38 +0100

    add django app files

---
+++ weddingshop/wedding_list/admin.py
@@ -0,0 +1,5 @@
+from django.contrib import admin
+from wedding_list.models import Product, WeddingList
+
+admin.site.register(Product)
+admin.site.register(WeddingList)
---
+++ weddingshop/wedding_list/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class WeddingListConfig(AppConfig):
+    name = "wedding_list"
