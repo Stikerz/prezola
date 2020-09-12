9540f94aac59c8790a1def3abb05108262c64a16
Stikerz <abiola_saraki@live.co.uk>
2020-09-12 04:17:22 +0100

    add login component

---
+++ src/src/app/components/login/login.component.ts
@@ -0,0 +1,48 @@
+import {Component, OnInit} from '@angular/core';
+import {UserService} from '../../services/user.service';
+import {ToastrService} from 'ngx-toastr';
+import {Router} from '@angular/router';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css'],
+  // providers: [UserService]
+})
+export class LoginComponent implements OnInit {
+
+  username = '';
+  password = '';
+  token: string;
+
+
+  constructor(private userService: UserService, private toastr: ToastrService, private router: Router) {
+  }
+
+  login() {
+    if (!this.username || !this.password) {
+      this.toastr.error('Error, please enter all fields', 'Error');
+    } else {
+      this.userService.login({
+        username: this.username,
+        password: this.password
+      });
+      this.clearFields();
+    }
+  }
+
+  clearFields() {
+     this.username = null;
+     this.password = null;
+  }
+  register() {
+    this.router.navigate(['register']);
+  }
+
+  ngOnInit(): void {
+    this.userService.sharedToken.subscribe(data => {
+      this.token = data;
+    });
+  }
+
+}
